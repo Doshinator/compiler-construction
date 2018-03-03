821f9f9c0197ee1779d42888525d42b7ae5c7f29
Doshi <36485291+Doshinator@users.noreply.github.com>
2018-03-03 13:38:09 -0500

    Add files via upload

--- basics.cpp
+++ basics.cpp
@@ -1,80 +1,97 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <iterator>
+#include <functional>
+#include <exception>      // std::exception
+#include <stdlib.h>
+
+
 using namespace std;
 /*
 ;; exp ::= int | (read) | (- exp) | (+ exp exp)
 ;; R0 ::= (program exp)
 */
 
+enum {R_Add, R_Mult, R_Div, R_And, R_XOR} binary_op;
+
+//uniquify ( map<var->var> , e)
 
 class Var{
   public:
-  void counterplusplus() { counter++;}
-  string label;
-  static int counter;
-  static void reset() {counter = 0;}
-  Var (string label) {this->label = label + "_" + to_string(counter++); }
-  Var (Var *base) {this->label = base->label + "_" + to_string(counter++); }
-  string toString() {return label;}
+    void counterplusplus() { counter++;}
+    string label;
+    static int counter;
+    static void reset() {counter = 0;}
+    Var (string label) {this->label = label + "_" + to_string(counter++); }
+    Var (Var *base) {this->label = base->label + "_" + to_string(counter++); }
+    string toString() {return label;}
 };
 int Var::counter = 0;
 
 class RExpr {
   public:
-  virtual string toString() = 0;
+    virtual string toString() = 0;
+    //virtual int eval(string::iterator in, map<Var,int> env) = 0 ;
 };
 
 class RNum : public RExpr{
   public:
-  int num;
-  RNum(int num) {this->num = num;}
-  string toString() { return to_string(num);}
+    int num;
+    RNum(int num) {this->num = num;}
+    string toString() { return to_string(num);}
+    //int eval(string::iterator in, map<Var,int> env){return num;}
 };
 
 class RRead : public RExpr {
   public:
-  RRead() { }
-  string toString() {return "(read)";}
-
+    RRead() { }
+    string toString() {return "(read)";}
+    //int eval(string::iterator in, map<Var,int> env){return atoi(in.next());}
 };
 
 class RNeg : public RExpr{
   public:
-  RExpr *arg;
-  RNeg (RExpr *arg) {this->arg = arg;}
-  string toString() { return "(- " + arg->toString() + ")";}
+    RExpr *arg;
+    RNeg (RExpr *arg) {this->arg = arg;}
+    string toString() { return "(- " + arg->toString() + ")";}
+    //int eval(string::iterator in, map<Var,int> env){return -1 * arg->eval(in, env);}
 };
 
 class RAdd : public RExpr{
   public:
-  RExpr *lhs;
-  RExpr *rhs;
-  RAdd(RExpr *lhs, RExpr *rhs) {this->lhs = lhs; this->rhs = rhs;}
-  string toString() { return "(+ " + lhs->toString() + " " + rhs->toString() + ")";}
-
+    RExpr *lhs;
+    RExpr *rhs;
+    RAdd(RExpr *lhs, RExpr *rhs) {this->lhs = lhs; this->rhs = rhs;}
+    string toString() { return "(+ " + lhs->toString() + " " + rhs->toString() + ")";}
+    //int eval(string::iterator in, map<Var,int> env){return lhs->eval(in, env) + rhs->eval(in, env);}
 };
 
 
 class RRef : public RExpr {
   public:
-  Var *x;
-  RRef( Var *x) {this->x = x;}
-  string toString() { return x->toString();}
-
+    Var *x;
+    RRef( Var *x) {this->x = x;}
+    string toString() { return x->toString();}
+    //int eval(string::iterator in, map<Var,int> env){return env->ref(x);}
 };
 
 class RLet : public RExpr{
   public:
-  Var *x;
-  RExpr *xe;
-  RExpr *be;
+    Var *x;
+    RExpr *xe;
+    RExpr *be;
 
-  RLet(Var *x, RExpr *xe, RExpr *be){
-    this->x = x; this->xe = xe; this->be = be;
-  }
-  string toString() { return "(let " + x->toString() + " " + xe->toString() + " " + be->toString() + ")";}
+    RLet(Var *x, RExpr *xe, RExpr *be){
+      this->x = x; this->xe = xe; this->be = be;
+    }
+    string toString() { return "(let " + x->toString() + " " + xe->toString() + " " + be->toString() + ")";}
+    /* int eval(string::iterator in, map<Var,int> env){ 
+      int what_is_x = xe->eval(in, env);
+      auto env_with_x = xe->set(x, what_is_x);
+      return be->eval(in, env_with_x);}
+    */
 };
-
 
 
 
@@ -90,16 +107,6 @@ class RBinop : public RExpr{
 */
 
 
-/*
-class Var {
-  public:
-  Var(string label) { this->label = label + "_" + counter++;}
-  Var(Var base) { this->label = base.label + "_" + counter++;}
-  string label;
-  static int counter = 0;
-  static void reset ( ) { count = 0; }
-};
-*/ 
 
 //returns new RAdd(n, n) where n is how many times it returns
 static RExpr *pow2(int n){
@@ -110,43 +117,106 @@ static RExpr *pow2(int n){
   }
 }
 
+
+static int test_passed = 0;
+
+static void summary(){
+    cout << test_passed << " test(s) passed!" << endl;
+}
+
+
+
+static void check(string which, std::function<int(void)> te, int expected){ //std::function takes function argument and lambda returns the function with type int
+    try{
+        int actual = te; // <-- te.get()?? wat 
+        if(actual != expected){
+            cout << which << ": got " << actual << ", expected " << expected << endl;
+            exit(1);
+        } else {
+            test_passed++;
+            }
+    }
+    catch (runtime_error *x){
+        cout << which << ": threw exception [" << x/*.what()*/ << "], expected " << expected << endl;
+        exit(1);
+    }
+
+}
+/*
+static void t(int ans, RExpr *e){
+    Var::reset();
+    cout << e->toString() << " =>";
+    check("eval", [] -> eval(e), ans); //lambda??
+}
+*/
+
+
+template <typename K, typename V>
+class IMap{
+    public:
+    map<K, V> hm;
+    IMap() { (new map<K, V>());}
+    
+      V ref(K x){
+        V r = hm.get(x);
+        if( r == NULL){
+            throw new runtime_error(x + " not found in hash map");
+        }
+        return r;
+    }
+
+    IMap<K, V> set(K x, V v){
+        map<K, V> hmp = new map<K, V>(hm);
+        hmp.put(x, v);
+        return new IMap<K, V>(hmp);
+    }
+
+    IMap<K, V> merge(IMap<K,V> o){
+        map<K, V> hmp = new map<K, V>(hm);
+        hmp.putAll(o.hm);
+        return new IMap<K, V>(hmp);
+    }
+
+    private:
+    IMap(map<K,V> hm){ this->hm = hm;}
+};
+
+
+static int eval(RExpr *e){
+  IMap<Var, int> *env = new IMap<Var, int> ();
+  return e->eval(env);
+}
+
+
+//like, lightning fast constructors
+static RExpr *I(int n){return new RNum(n);}
+static RExpr *N(RExpr *e){return new RNeg(e);}
+static RExpr *A(RExpr *lhs, RExpr *rhs) {return new RAdd(lhs, rhs);}
+static RExpr *R(Var *x){return new RRef(x);}
+static RExpr *Let(Var *x, RExpr *xe, RExpr *be) {return new RLet(x, xe, be);}
+
+
+static void t(int ans, RExpr *e){
+  Var::reset();
+  cout << e->toString() << " =>";
+  check("eval", []() -> {return eval(e)}, ans); //lambda?? supposed to go through the function eval with argument e and return type int.
+}
+
+
 int main(void){
-
-  RExpr* exp1 = new RNum(5);
-  cout << exp1->toString();
-
-  RExpr *exp2 = new RRead();
-  cout << exp2->toString();
   
-  RExpr *exp3 = new RNeg(new RNum(5));
-  cout << exp3->toString() << endl;
-
-  RExpr *exp4 = (new RAdd(new RNum(4), new RAdd(new RNum(5), new RNum(6))));
-  cout << exp4->toString() << endl;
-
-  RExpr *exp5 = new RNeg(new RAdd(new RNum(4), new RAdd(new RNum(5), new RNum(6))));
-  cout << exp5->toString() << endl;
+  //cout << pow2(3)->toString() << endl;
 
   Var *x = new Var("x");
+  RExpr *ex1 = Let(x, I(8),
+                  A(Let(x, I(10), R(x)),R(x)));
+  cout << ex1->toString() << endl;
 
-  RExpr *ex1 =
-          new RLet(x , new RNum(10),
-                  new RAdd( new RNeg( new RRef(x)), new RRead()));
-  cout << ex1->toString()<< endl;
+  t(5, I(5));
+  RExpr *exp2 = A(I(10), N(A(I(5), I(6))));
+  cout << exp2->toString() << endl;
 
-  RExpr *timmy = 
-           new RLet(x, new RNum(10), 
-                  new RAdd(new RNum(5), new RRef(x)));
-  cout << "timmy = " << timmy->toString() << endl;
 
-  RExpr *test = new RNeg(
-                  new RNeg( 
-                    new RAdd( new RNum(18), new RNeg( 
-                      new RAdd(new RNum(5),new RNum(5))))));
-  cout << "test = " << test->toString() << endl;
 
-  
-  cout << pow2(2)->toString() << endl;
-  
   return 0;
 }
